b90074a5d5365260bacae00ae8ac4532764b0082
jcromerohdz <jcromerohdz@gmail.com>
2020-01-02 14:46:07 -0800

    More devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,13 @@
 Rails.application.routes.draw do
 
   as :user do
-    put '/user/confirmation' => 'confirmations#update', :via => patch, :as => :update_user_confirmation
+    put '/user/confirmation' => 'confirmations#update', :via => :patch, :as => :update_user_confirmation
   end
 
-  end
-  devise_for :users, controllers: {
-    registrations: 'registrations',
-    confirmations: ''
-  }
+ devise_for :users, controllers: {
+  registrations: 'registrations',
+  confirmations: 'confirmations'
+}
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
