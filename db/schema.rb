5b3d44ddb0df893c0b0a55d219928fa393efa44e
jcromerohdz <jcromerohdz@gmail.com>
2020-01-03 11:58:05 -0800

    Performing dashboard and home page UI, also initial setup for teams

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_02_223527) do
+ActiveRecord::Schema.define(version: 2020_01_03_195438) do
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
 
 # Could not dump table "users" because of following StandardError
 #   Unknown type 'strings' for column 'name'
